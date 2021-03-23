d3f62d4498df000986af7c71fd8f2477484cc234
Dania <dania.liu@ocsbstudent.ca>
2021-03-23 16:52:59 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Links buttons to JavaScript.
+document.getElementById('button').addEventListener('click', comparison)
+let userInput = 0
+
+function comparison() {
+  // Converts user input to an integer.
+  userInput = document.getElementById('input').value
+  // Checks if user input is less than 0.
+  if (userInput < 0) {
+    document.getElementById('answer').innerHTML = "Negative"
+  } else {
+    document.getElementById('answer').innerHTML = "Positive"
+  }
+}
