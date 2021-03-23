e506cfe1ef77d6a95df6d5173277a4b3674a38bc
Dania <dania.liu@ocsbstudent.ca>
2021-03-23 16:59:50 +0000

    Fixed JS

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function comparison() {
   userInput = document.getElementById('input').value
   // Checks if user input is less than 0.
   if (userInput < 0) {
-    document.getElementById('answer').innerHTML = "Negative"
+    document.getElementById('answer').innerHTML = 'Negative'
   } else {
-    document.getElementById('answer').innerHTML = "Positive"
+    document.getElementById('answer').innerHTML = 'Positive'
   }
 }
